31b9ceb018d1240a782a78661c6c2d7fe30c5795
anfebeba2001 <57334505+anfebeba2001@users.noreply.github.com>
2024-12-03 12:12:27 -0500

    Fixing folder 2

---
+++ src/test/java/com/AndresBejarano/PokeAPIVol2/PokeApiVol2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AndresBejarano.PokeAPIVol2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiVol2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
